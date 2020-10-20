2bf39c7e16cd4189c24b8bbd8b965e1c1d964963
Yadong Zhu <yadong.zhu@gmail.com>
2020-10-20 09:08:31 +0800

    add gitignore

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-let port = "2021"
+require("dotenv").config();
+let port = process.env.APP_PORT;
 const http = require('http');
-// const fs = require('fs');
 const express = require('express');
 const app = express();
-// const options = {
-//   key: fs.readFileSync('server.key'),
-//   cert: fs.readFileSync('server.cert')
-// };
-// app.use('/', require('redirect-https')({}));
+
 app.use('/help', express.static(__dirname + '/html'));
 app.use('/help/js', express.static(__dirname + '/js'));
 app.use('/help/css', express.static(__dirname + '/css'));
 app.use('/help/img', express.static(__dirname + '/img'));
-
-app.get('/hello', (req, res) => {
-  res.send('Hello HTTPS!')
-})
 
 http.createServer(app).listen(port, () => {
   console.log('HTTP Listening in port '+port+"!")
